ecfd765c115f3a347124c89ea10618a5fa6245e5
Deny sPokatilov <denyspokatilov@gmail.com>
2017-02-12 17:37:05 +0200

    commit

--- php_basic_array/16_test.php
+++ php_basic_array/16_test.php
@@ -5,15 +5,14 @@
  * Date: 12.02.2017
  * Time: 0:08
  */
+$count=0;
 $arr=[1, 2, 3, 4, 5, 6, 7, 8, 9];
 foreach ($arr as $i) {
-        echo $i;
-        for ($j = $i; $j<4; $j++) {
-        if ($j > 3) {
-
-            echo '</br>';}
+    $count++;
+        echo $i.",";
+        if($count%3==0){
+            echo '<br>';
         }
-
         }
 
 
--- php_basic_array/22_test.php
+++ php_basic_array/22_test.php
@@ -4,4 +4,11 @@
  * User: User
  * Date: 12.02.2017
  * Time: 2:49
- */
+ */for ($i=0; $i<=5; $i++)
+{
+    for ($j=0; $j<$i; $j++)
+    {
+        echo '**';
+    }
+    echo "<pre>";
+}
