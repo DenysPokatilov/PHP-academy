91786a22b40b186b3aa81375df9f4cd8de2d0750
Deny sPokatilov <denyspokatilov@gmail.com>
2017-02-13 02:24:28 +0200

    все сделано кроме 27(подсмотрел)

---
+++ php_basic_array/28_task.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 13.02.2017
+ * Time: 2:22
+ */
+for ($i=1;$i<10;$i++)
+{for ($j=1;$j<10;$j++)
+{
+    $res=$i*$j;
+    echo '<pre>'. "$i x $j = $res</br>";
+}
+echo '<pre>';
+    echo '</br>';
+}
